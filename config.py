6843e4278578b7550731b3ead098dcdd3c583f94
MyrnaC <66696946+MyrnaC@users.noreply.github.com>
2021-07-18 19:14:11 -0500

    BatchN+Anchors+IOU
    Se realiza la modificación de la arquitectura, agregando batch normalization, anchors, nueva arquitecura y cambiando confidence function

--- config.py
+++ config.py
@@ -1,41 +1,35 @@
-#-*- coding: utf-8 -*-
 import argparse
-
-def str2bool(v):
-  return v.lower() in ('true', '1')
 
 arg_lists = []
 parser = argparse.ArgumentParser()
 
+def str2bool(v):
+	return v.lower() in ('true', '1')
+
 def add_argument_group(name):
-  arg = parser.add_argument_group(name)
-  arg_lists.append(arg)
-  return arg
+	arg = parser.add_argument_group(name)
+	arg_lists.append(arg)
+	return arg
 
 # Network
 net_arg = add_argument_group('Network')
 
 # Data
 data_arg = add_argument_group('Data')
-
 data_arg.add_argument('--data_set_dir', type=str, default='data/UnrealDataset')
-#data_arg.add_argument('--dataset', type=str, default='UnrealDataset', choices=['UnrealDataset', 'Satellite'])
 data_arg.add_argument('--use_subsampled', type=str2bool, default=False)
 data_arg.add_argument('--compute_mean', type=str2bool, default=False)
-
 data_arg.add_argument('--data_main_dir', type=str, default='')
-data_arg.add_argument('--data_train_dirs', type=eval, nargs='+', default=['00_D','01_D','02_D','03_D','04_D','05_D','06_D','07_D','08_D','10_D','11_D','13_D','15_D','16_D','17_D','18_D','19_D','20_D'])
-data_arg.add_argument('--data_test_dirs', type=eval, nargs='+', default=['09_D','14_D'])
-
-data_arg.add_argument('--input_height', type=int, default=160)#orig 376
-data_arg.add_argument('--input_width', type=int, default=256)#orig 1240
-
+data_arg.add_argument('--data_train_dirs', type=eval, nargs='+', default=['00_D', '01_D', '02_D', '03_D', '04_D', '05_D', '06_D', '07_D', '08_D', '10_D', '11_D', '13_D', '15_D', '16_D', '17_D', '18_D', '19_D', '20_D'])
+data_arg.add_argument('--data_test_dirs', type=eval, nargs='+', default=['23_D'])
+data_arg.add_argument('--input_height', type=int, default=160)
+data_arg.add_argument('--input_width', type=int, default=256)
 data_arg.add_argument('--input_channel', type=int, default=3)
 data_arg.add_argument('--img_extension', type=str, default="png")
 
 #JMOD2 param
 jmod2_arg = add_argument_group('JMOD2')
-jmod2_arg.add_argument('--detector_confidence_thr', type=int, default=0.65)
+jmod2_arg.add_argument('--detector_confidence_thr', type=int, default=0.2)
 
 #CADENA param
 cad_arg = add_argument_group('Cadena')
@@ -45,10 +39,9 @@ cad_arg.add_argument('--cadena_resize_out', type=str2bool, default=True)
 cad_arg.add_argument('--rgb_ae_channel_override', type=str2bool, default=False)
 cad_arg.add_argument('--rgb_ae_channel', type=int, default=2)
 
-
 # Training / test parameters
 train_arg = add_argument_group('Training')
-train_arg.add_argument('--is_train', type=str2bool, default=False, help='') #Set True for training
+train_arg.add_argument('--is_train', type=str2bool, default=True, help='') #Set True for training
 train_arg.add_argument('--exp_name', type=str, default='NAME_OF_EXPERIMENT')
 train_arg.add_argument('--preload_ram',type=str2bool, default=False)
 train_arg.add_argument('--use_augmentation', type=str2bool, default=True, help='')
@@ -56,23 +49,21 @@ train_arg.add_argument('--validation_split', type=float, default=0.2, help='')
 train_arg.add_argument('--max_step', type=int, default=10000, help='')
 train_arg.add_argument('--batch_size', type=int, default=32, help='')
 train_arg.add_argument('--buffer_size', type=int, default=25600, help='')
-train_arg.add_argument('--num_epochs', type=int, default=60, help='')
+train_arg.add_argument('--num_epochs', type=int, default=100, help='')
 train_arg.add_argument('--random_seed', type=int, default=123, help='')
-train_arg.add_argument('--learning_rate', type=float, default=1e-5, help='')
-
-train_arg.add_argument('--weights_path', type=str, default="") #Used for finetuning or to resume training
+train_arg.add_argument('--learning_rate', type=float, default=1e-3, help='')
+train_arg.add_argument('--weights_path', type=str, default="weights/jmod2.hdf5") #Used for finetuning or to resume training
 train_arg.add_argument('--resume_training',type=str2bool, default=False)
 
 # Misc
 misc_arg = add_argument_group('Misc')
-misc_arg.add_argument('--is_deploy', type=str2bool, default=True, help='')
+misc_arg.add_argument('--is_deploy', type=str2bool, default=False, help='')
 misc_arg.add_argument('--log_step', type=int, default=20, help='')
 misc_arg.add_argument('--log_dir', type=str, default='logs') #DIRECTORY WHERE TO SAVE MODEL CHECKPOINTS
 misc_arg.add_argument('--debug', type=str2bool, default=True)
-misc_arg.add_argument('--gpu_memory_fraction', type=float, default=0.5)
+misc_arg.add_argument('--gpu_memory_fraction', type=float, default=0.8)
 misc_arg.add_argument('--max_image_summary',type=int, default=4)
 
-
 def get_config():
-  config, unparsed = parser.parse_known_args()
-  return config, unparsed
+	config, unparsed = parser.parse_known_args()
+	return config, unparsed
--- evaluate_on_unrealdataset.py
+++ evaluate_on_unrealdataset.py
@@ -1,29 +1,29 @@
 import cv2
+import os
+from glob import glob
 import numpy as np
 import lib.EvaluationUtils as EvaluationUtils
 from config import get_config
-import os
-from glob import glob
 from lib.Evaluators import JMOD2Stats
 
-
 def preprocess_data(rgb, gt, seg, w=256, h=160, crop_w=0, crop_h=0, resize_only_rgb = False):
-    crop_top = np.floor((rgb.shape[0] - crop_h) / 2).astype(np.uint8)
-    crop_bottom = rgb.shape[0] - np.floor((rgb.shape[0] - crop_h) / 2).astype(np.uint8)
-    crop_left = np.floor((rgb.shape[1] - crop_w) / 2).astype(np.uint8)
-    crop_right = rgb.shape[1] - np.floor((rgb.shape[1] - crop_w) / 2).astype(np.uint8)
+	crop_top = np.floor((rgb.shape[0] - crop_h) / 2).astype(np.uint8)
+	crop_bottom = rgb.shape[0] - np.floor((rgb.shape[0] - crop_h) / 2).astype(np.uint8)
+	crop_left = np.floor((rgb.shape[1] - crop_w) / 2).astype(np.uint8)
+	crop_right = rgb.shape[1] - np.floor((rgb.shape[1] - crop_w) / 2).astype(np.uint8)
+	
+	rgb = np.asarray(rgb, dtype=np.float32) / 255.
+	rgb = cv2.resize(rgb, (w, h), cv2.INTER_LINEAR)
+	rgb = np.expand_dims(rgb, 0)
+	gt = np.asarray(gt, dtype=np.float32)
 
-    rgb = np.asarray(rgb, dtype=np.float32) / 255.
-    rgb = cv2.resize(rgb, (w, h), cv2.INTER_LINEAR)
-    rgb = np.expand_dims(rgb, 0)
-    gt = np.asarray(gt, dtype=np.float32)
+	if not resize_only_rgb:
+		gt = cv2.resize(gt, (w, h), cv2.INTER_NEAREST)
+		seg = cv2.resize(seg, (w, h), cv2.INTER_NEAREST)
 
-    if not resize_only_rgb:
-        gt = cv2.resize(gt, (w, h), cv2.INTER_NEAREST)
-    gt = EvaluationUtils.depth_to_meters_airsim(gt)
-    if not resize_only_rgb:
-        seg = cv2.resize(seg, (w, h), cv2.INTER_NEAREST)
-    return rgb, gt, seg
+	gt = EvaluationUtils.depth_to_meters_airsim(gt)
+
+	return rgb, gt, seg
 
 #edit config.py as required
 config, unparsed = get_config()
@@ -33,7 +33,7 @@ model_name = 'jmod2'
 
 model, detector_only = EvaluationUtils.load_model(model_name, config)
 
-showImages = False
+showImages = True
 
 dataset_main_dir = config.data_set_dir
 test_dirs = config.data_test_dirs
@@ -41,49 +41,68 @@ test_dirs = config.data_test_dirs
 #compute_depth_branch_stats_on_obs is set to False when evaluating detector-only models
 jmod2_stats = JMOD2Stats(model_name, compute_depth_branch_stats_on_obs=not detector_only)
 
-
 for test_dir in test_dirs:
-    depth_gt_paths = sorted(glob(os.path.join(dataset_main_dir, test_dir, 'depth', '*' + '.png')))
-    rgb_paths = sorted(glob(os.path.join(dataset_main_dir, test_dir, 'rgb', '*' + '.png')))
-    seg_paths = sorted(glob(os.path.join(dataset_main_dir, test_dir, 'segmentation', '*' + '.png')))
+	depth_gt_paths = sorted(glob(os.path.join(dataset_main_dir, test_dir, 'depth', '*' + '.png')))
+	rgb_paths = sorted(glob(os.path.join(dataset_main_dir, test_dir, 'rgb', '*' + '.png')))
+	seg_paths = sorted(glob(os.path.join(dataset_main_dir, test_dir, 'segmentation', '*' + '.png')))
+	labels_paths = sorted(glob(os.path.join(dataset_main_dir, test_dir, 'obstacles_30m', '*' + '.txt')))
+	index = 0
 
+	for gt_path, rgb_path, seg_path, label_path in zip(depth_gt_paths, rgb_paths, seg_paths, labels_paths):
+		index_str = str(index)
+		rgb_raw = cv2.imread(rgb_path)
+		gt = cv2.imread(gt_path, 0)
+		seg = cv2.imread(seg_path, 0)
+		obs_gt = (open(label_path)).readlines()
 
-    for gt_path, rgb_path, seg_path in zip(depth_gt_paths, rgb_paths, seg_paths):
+		#Normalize input between 0 and 1, resize if required
+		rgb, gt, seg = preprocess_data(rgb_raw, gt, seg, w=config.input_width,h=config.input_height, resize_only_rgb = True)
 
-        rgb_raw = cv2.imread(rgb_path)
-        gt = cv2.imread(gt_path, 0)
-        seg = cv2.imread(seg_path, 0)
+		#Forward pass to the net
+		results = model.run(rgb)
 
-        #Normalize input between 0 and 1, resize if required
+		if results[0] is not None:
+			depth_raw = results[0].copy()
+			pred_depth_ = results[0][0, :, :, 0].copy()
+			depth_gt = gt.copy()
+			#evaluate only on valid predictions (some methods like Cadena's may return zero or negative values)
+			depth_gt[np.nonzero(pred_depth_ <= 0)] = 0.0
+		else:
+			depth_gt = None
+		#Corrected depth
+		if results[2] is not None:
+			corr_depth = results[2][0, :, :, 0].copy()
+			corr_depth[np.nonzero(depth_gt <= 0)] = 0.0
+			results[2] = corr_depth
+		#Get obstacles from GT segmentation and depth
+		#gt_obs = EvaluationUtils.get_obstacles_from_seg_and_depth(gt, seg, segm_thr=-1)
+		obstacles_list = []
+		for obs in obs_gt:
+			obstacle = obs.split(' ')
+			x_cell = int(obstacle[0])
+			y_cell = int(obstacle[1])
+			x = 32. * (float(obstacle[2]) + x_cell)
+			y = 32. * (float(obstacle[3]) + y_cell)
+			w = float(obstacle[4]) * 256.
+			h = float(obstacle[5]) * 160.
+			x = x - (w / 2.)
+			y = y - (h / 2.)
+			if x < 0.:
+				x = 0.
+			if y < 0.:
+				y = 0.
+			mean = float(obstacle[6]) * 39.75
+			var = float(obstacle[7]) * 39.75 * 100
+			obstacles_list.append([(x, y, w, h), (mean, var)])
+		gt_obs = EvaluationUtils.get_obstacles_from_list(obstacles_list)
 
-        rgb, gt, seg = preprocess_data(rgb_raw, gt, seg, w=config.input_width,h=config.input_height, resize_only_rgb = True)
+		if showImages:
+			if results[1] is not None:
+				EvaluationUtils.show_detections(rgb_raw, results[1], gt_obs, file_name=index_str.zfill(5)+'.png', sleep_for=10)
+			if results[0] is not None:
+				EvaluationUtils.show_depth(rgb_raw, depth_raw, gt, file_name=index_str.zfill(5)+'.png', sleep_for=10)
 
-        #Forward pass to the net
-        results = model.run(rgb)
-
-        if results[0] is not None:
-            depth_raw = results[0].copy()
-            pred_depth_ = results[0][0, :, :, 0].copy()
-            depth_gt = gt.copy()
-            #evaluate only on valid predictions (some methods like Cadena's may return zero or negative values)
-            depth_gt[np.nonzero(pred_depth_ <= 0)] = 0.0
-        else:
-            depth_gt = None
-        #Corrected depth
-        if results[2] is not None:
-            corr_depth = results[2][0, :, :, 0].copy()
-            corr_depth[np.nonzero(depth_gt <= 0)] = 0.0
-            results[2] = corr_depth
-
-        #Get obstacles from GT segmentation and depth
-        gt_obs = EvaluationUtils.get_obstacles_from_seg_and_depth(gt, seg, segm_thr=-1)
-
-        if showImages:
-            if results[1] is not None:
-                EvaluationUtils.show_detections(rgb_raw, results[1], gt_obs, sleep_for=10)
-            if results[0] is not None:
-                EvaluationUtils.show_depth(rgb_raw, depth_raw, gt,sleep_for=10)
-
-        jmod2_stats.run(results, [depth_gt, gt_obs])
+		jmod2_stats.run(results, [depth_gt, gt_obs])
+		index += 1
 
 results = jmod2_stats.return_results()
